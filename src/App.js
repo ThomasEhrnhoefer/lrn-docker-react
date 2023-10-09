fa5c0ad4bf815fe1c0afa09b81c64212e5aa63d0
Thomas Ehrnhöfer <thomas.ehrnhoefer@planview.com>
2023-10-09 14:23:05 +0200

    change app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React. Do it!
         </a>
       </header>
     </div>
